477c0c855c7d7f155d51081e9e45c4a09a11ef5a
MyopicMuppet <47373110+MyopicMuppet@users.noreply.github.com>
2019-03-25 12:06:21 +1100

    class update

---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    #region Variables
+    public float maxHP = 100, curHP = 100;
+    public Slider healthBar;
+    public Canvas myCanvas;
+    #endregion
+    #region Canvas & HealthBar
+
+
+    //attach healthbar and canvas element
+    private void Start()
+    {
+        myCanvas = transform.Find("Canvas").GetComponent<Canvas>();
+        healthBar = myCanvas.transform.Find("Slider").GetComponent<Slider>();
+
+    }
+    #endregion
+    #region Camera
+    //clamp camera to camera angle
+    private void Update()
+    {
+        //sets between min/max value between 0 & 1
+        healthBar.value = Mathf.Clamp01(curHP / maxHP);
+        //canvas position rotates to match camera
+        myCanvas.transform.LookAt(Camera.main.transform);
+    }
+    #endregion
+}
+
+
+
+
+
+
+
