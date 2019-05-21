c06b3a2946bfc8199288d6609a7e7e2d4b791b9c
MyopicMuppet <47373110+MyopicMuppet@users.noreply.github.com>
2019-05-21 11:57:49 +1000

    end of class

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,147 @@
+﻿//in this script  you will only need using UnityEngine as we just need the script to connect to unity
+using UnityEngine;
+//this public class doent inherit from MonoBehaviour
+//this script is also referenced by other scripts but never attached to anything
+public class Item
+{
+    //basic variables for items that we need are 
+    #region Private Variables
+    //Identification Number
+    private int _idNum;
+    //Object Name
+    private string _name;
+    //Value of the Object
+    private int _value;
+    //Description of what the Object is
+    private string _description;
+    //Icon that displays when that Object is in an Inventory
+    private Texture2D _icon;
+    //Mesh of that object when it is equipt or in the world
+    private GameObject _mesh;
+    //Enum ItemType which is the Type of object so we can classify them
+    private ItemType _type;
+    private int _heal;
+    private int _damage;
+    private int _armour;
+    private int _amount;
+    #endregion
+    #region Constructors
+    //A constructor is a bit of code that allows you to create objects from a class. You call the constructor by using the keyword new 
+    //followed by the name of the class, followed by any necessary parameters.
+    //the Item needs Identification Number, Object Name, Icon and Type
+    //here we connect the parameters with the item variables
+
+    public void ItenCon(int itemId, string itemName, Texture2D itemIcon, ItemType itemType)
+    {
+        _idNum = itemId;
+        _name = itemName;
+        _icon = itemIcon;
+        _type = itemType;
+    }
+    #endregion
+    #region Public Variables
+    //here we are creating the public versions or our private variables that we can reference and connect to when interacting with items
+    //public Identification Number 
+
+        public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+    public int ID
+    {
+        get { return _idNum; }
+        set { _idNum = value; }
+    }
+    //get the private Identification Number
+    //and set it to the value of our public Identification Number
+    //public Name 
+    public int Value
+    {
+        //get the private Name
+        get { return _value; }
+        set { _value = value; }
+
+    }
+    //and set it to the value of our public Name
+
+    //public Value 
+
+    public string Description
+    {
+        //public Description 
+        //get the private Description
+        //and set it to the value of our public Description
+
+        get { return _description; }
+        set { _description = value; }
+    }
+    //get the private Value
+    //and set it to the value of our public Value
+    //public Icon 
+
+    public Texture2D Icon
+    {
+        //get the private Icon
+        get { return _icon; }
+        set { _icon = value; }
+    }
+    //and set it to the value of our public Icon
+    //public Mesh 
+    public GameObject Mesh
+    {
+        get { return _mesh; }
+        set { _mesh = value; }
+    }
+    //get the private Mesh
+    //and set it to the value of our public Mesh
+    //public Type 
+    //get the private Type
+
+    public ItemType Type
+    {
+        get { return _type; }
+        set { _type = value; }
+    }
+    //and set it to the value of our public Type
+
+    public int Heal
+    {
+        get { return _heal;  }
+        set { _heal = value; }
+    }
+
+    public int Damage
+    {
+        get { return _damage; }
+        set { _damage = value;  }
+    }
+
+    public int Armour
+    {
+        get { return _armour; }
+        set { _armour = value; }
+    }
+
+    public int Amount
+    {
+        get { return _amount; }
+        set { _amount = value; }
+    }
+    #endregion
+}
+#region Enums
+//The Global Enum ItemType that we have created categories in
+public enum ItemType
+{
+    Food,
+    Weapon,
+    Apparel,
+    Crafting,
+    Quest,
+    Money,
+    Ingredients,
+    Potions,
+    Scrolls
+}
+#endregion
---
+++ Assets/Scripts/Items/ItemData.cs
@@ -0,0 +1,323 @@
+﻿using UnityEngine;
+
+public static class ItemData
+{
+    public static Item CreateItem(int itemID)
+    {
+        string name = "";
+        int value = 0;
+        string description = "";
+        string icon = "";
+        string mesh = "";
+        ItemType type = ItemType.Food;
+        int heal = 0;
+        int damage = 0;
+        int armour = 0;
+        int amount = 0;
+
+        switch (itemID)
+        {
+            #region Food 0-99
+            case 0:
+                name = "Meat";
+                value = 15;
+                description = "It comes from animals";
+                icon = "Meat_Icon";
+                mesh = "Meat Mesh";
+                type = ItemType.Food;
+                heal = 17;
+                amount = 1;
+                break;
+
+            case 1:
+                name = "Chicken";
+                value = 10;
+                description = "From a bird";
+                icon = "Chicken_Icon";
+                mesh = "Chicken Mesh";
+                type = ItemType.Food;
+                heal = 10;
+                amount = 1;
+                break;
+
+            case 2:
+                name = "Bread";
+                value = 1;
+                description = "Have daily";
+                icon = "Bread_Icon";
+                mesh = "Bread Mesh";
+                type = ItemType.Food;
+                heal = 5;
+                amount = 1;
+                break;
+
+
+            #endregion
+            #region Weapon 100-199
+            case 100:
+                name = "Short Sword";
+                value = 25;
+                description = "Stick em with the pointy end";
+                icon = "ShorSword_Icon";
+                mesh = "ShortSword Mesh";
+                type = ItemType.Weapon;
+                damage = 15;
+                amount = 1;
+                break;
+
+            case 101:
+                name = "Long Sword";
+                value = 50;
+                description = "Size matters";
+                icon = "LongSword_Icon";
+                mesh = "LongSword Mesh";
+                type = ItemType.Weapon;
+                damage = 25;
+                amount = 1;
+                break;
+
+            case 102:
+                name = "Axe";
+                value = 75;
+                description = "Big and beasty";
+                icon = "Axe_Icon";
+                mesh = "Axe Mesh";
+                type = ItemType.Weapon;
+                damage = 30;
+                amount = 1;
+                break;
+
+            #endregion
+            #region Apparel 200-299
+            case 200:
+                name = "Peasant Cap";
+                value = 15;
+                description = "For the simple life";
+                icon = "PeasantHat_Icon";
+                mesh = "PeasantHat Mesh";
+                type = ItemType.Apparel;
+                armour = 2;
+                amount = 1;
+                break;
+
+            case 201:
+                name = "Steel Helm";
+                value = 50;
+                description = "A pot for your noggin";
+                icon = "SteelHelm_Icon";
+                mesh = "SteelHelm Mesh";
+                type = ItemType.Apparel;
+                armour = 10;
+                amount = 1;
+                break;
+
+            case 202:
+                name = "Great Helm";
+                value = 100;
+                description = "A lordly helm";
+                icon = "GreatHelm_Icon";
+                mesh = "GreatHelm Mesh";
+                type = ItemType.Apparel;
+                armour = 20;
+                amount = 1;
+                break;
+
+            #endregion
+            #region Crafting 300-399
+            case 300:
+                name = "Spring";
+                value = 2;
+                description = "A metal spring";
+                icon = "Spring_Icon";
+                mesh = "Spring Mesh";
+                type = ItemType.Crafting;
+                amount = 1;
+                break;
+
+            case 301:
+                name = "Mirror";
+                value = 4;
+                description = "Don't break its bad luck";
+                icon = "Mirror_Icon";
+                mesh = "Mirror Mesh";
+                type = ItemType.Crafting;
+                amount = 1;
+                break;
+
+            case 302:
+                name = "Nut";
+                value = 1;
+                description = "essential for anything that is supposed to stay together";
+                icon = "Nut_Icon";
+                mesh = "Nut Mesh";
+                type = ItemType.Crafting;
+                amount = 1;
+                break;
+
+            #endregion
+            #region Quest 400-499
+
+            case 400:
+                name = "Crystal Ball";
+                value = 150;
+                description = "Can you see the future yet";
+                icon = "CrystalBall_Icon";
+                mesh = "CrystalBall Mesh";
+                type = ItemType.Quest;
+                amount = 1;
+                break;
+
+            case 401:
+                name = "Staff";
+                value = 240;
+                description = "More than just a walking aid";
+                icon = "Staff_Icon";
+                mesh = "Staff Mesh";
+                type = ItemType.Quest;
+                amount = 1;
+                break;
+
+            case 402:
+                name = "Tome";
+                value = 485;
+                description = "Where knowledge is stored";
+                icon = "Tome_Icon";
+                mesh = "Tome Mesh";
+                type = ItemType.Quest;
+                amount = 1;
+                break;
+            #endregion
+            #region Currency 500-599
+            #endregion
+            #region Ingredients 600-699
+
+            case 600:
+                name = "Blueberries";
+                value = 3;
+                description = "Taken from bushes";
+                icon = "Blueberries_Icon";
+                mesh = "Blueberries Mesh";
+                type = ItemType.Ingredients;
+                amount = 1;
+                break;
+
+            case 601:
+                name = "Mushroom";
+                value = 2;
+                description = "Grows in much";
+                icon = "Mushroom_Icon";
+                mesh = "Mushroom Mesh";
+                type = ItemType.Ingredients;
+                amount = 1;
+                break;
+
+            case 602:
+                name = "Flower";
+                value = 5;
+                description = "Its pretty";
+                icon = "Flower_Icon";
+                mesh = "Flower Mesh";
+                type = ItemType.Ingredients;
+                amount = 1;
+                break;
+            #endregion
+            #region Potions 700-799
+
+            case 700:
+                name = "Green Health";
+                value = 25;
+                description = "a little pick me up";
+                icon = "GreenHealth_Icon";
+                mesh = "GreenHealth Mesh";
+                type = ItemType.Potions;
+                heal = 25;
+                amount = 1;
+                break;
+
+            case 701:
+                name = "Blue Health";
+                value = 75;
+                description = "For Broken arms and punctured Spleens";
+                icon = "BlueHealth_Icon";
+                mesh = "BlueHealth Mesh";
+                type = ItemType.Potions;
+                heal = 50;
+                amount = 1;
+                break;
+
+            case 702:
+                name = "Red Health";
+                value = 150;
+                description = "For near death experiences";
+                icon = "RedHealth_Icon";
+                mesh = "RedHealth Mesh";
+                type = ItemType.Potions;
+                heal = 80;
+                amount = 1;
+                break;
+            #endregion
+            #region Scrolls 800-899
+
+            case 800:
+                name = "Blank Scroll";
+                value = 2;
+                description = "It says nothing";
+                icon = "BlankScroll_Icon";
+                mesh = "BlankScroll Mesh";
+                type = ItemType.Scrolls;
+                amount = 1;
+                break;
+
+            case 801:
+                name = "Historical Scroll";
+                value = 150;
+                description = "Brief account of local history";
+                icon = "HistoricalScroll_Icon";
+                mesh = "HistoricalScroll Mesh";
+                type = ItemType.Scrolls;
+                amount = 1;
+                break;
+
+            case 802:
+                name = "Shipping Manifest";
+                value = 50;
+                description = "A list of items being sent to far away places";
+                icon = "ShippingManifest_Icon";
+                mesh = "ShippingManifest Mesh";
+                type = ItemType.Scrolls;
+                amount = 1;
+                break;
+            #endregion
+
+            default:
+                itemID = 4;
+                name = "Apple";
+                value = 1;
+                description = "Muchie and crunchy";
+                icon = "Apple_Icon";
+                mesh = "Apple Mesh";
+                type = ItemType.Food;
+                heal = 5;
+                amount = 1;
+                break;
+
+        }
+        Item temp = new Item
+        {
+            Name = name,
+            Description = description,
+            ID = itemID,
+            Value = value,
+            Damage = damage,
+            Armour = armour,
+            Amount = amount,
+            Heal = heal,
+            Type = type,
+            Mesh = Resources.Load("Prefabs/" + mesh) as GameObject,
+            Icon = Resources.Load("Icon/" + icon) as Texture2D
+
+
+        };
+        return temp;
+    }
+}
